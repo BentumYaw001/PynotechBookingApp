312069a83b309f20d66a6644304ed0d5159d452a
BentumYaw001 <richmondbentum2020@gmail.com>
2025-02-03 13:20:35 +0000

    project files updated

---
+++ src/components/SplashScreen.jsx
@@ -0,0 +1,13 @@
+import yb from "/src/assets/images/yb.png";
+
+function SplashScreen() {
+  return (
+    <>
+      <div className="Splash">
+        <img src={yb} alt="" />
+      </div>
+    </>
+  );
+}
+
+export default SplashScreen;
